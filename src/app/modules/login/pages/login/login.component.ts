cdaf4ef4e1e096108fad262bef28ba9142a80362
Tiago <lima.tiagom@gmail.com>
2022-06-01 06:36:51 -0300

    Add redirect to home page if user is logged in

--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
+    if (this.userService.isLoggedIn()) this.router.navigate(['home']);
   }
-
 }
