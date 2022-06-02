29946f75f961fc7a1f670d51603248cecd5ea8ba
Tiago <lima.tiagom@gmail.com>
2022-06-02 08:53:07 -0300

    Refactor some structuring and fix minor bugs

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,9 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [HeaderComponent, MenuComponent],
