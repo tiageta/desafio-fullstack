dfaecef727c2efc07591a43e976e97f1d40e52bf
Tiago <lima.tiagom@gmail.com>
2022-07-28 20:13:24 -0300

    Configure heroku mysql db

--- backend/express/config/db-connection.js
+++ backend/express/config/db-connection.js
@@ -1,11 +1,10 @@
 const db = require("mysql2");
 
 const connection = db.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "ford_api",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA,
 });
 
 module.exports = connection;
