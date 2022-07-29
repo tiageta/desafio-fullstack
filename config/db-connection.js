a3296c512484c37d3c749275f6395b5d77cbbfa8
Tiago <lima.tiagom@gmail.com>
2022-07-29 04:15:25 -0300

    Add db config as env variable

--- config/db-connection.js
+++ config/db-connection.js
@@ -1,10 +1,10 @@
 const db = require("mysql2");
 
 const connection = db.createPool({
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "ba7ec75a83a7c1",
-  password: "7eaeef76",
-  database: "heroku_c77450bd68ea0f2",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA,
 });
 
 module.exports = connection;
